9880ae9fb89ce55c2b5976ad7ea2ee22652311f0
Andrew Ptasinskij <aptasinskij@gmail.com>
2018-09-05 15:50:03 +0300

    moved application domain events listener to separate package

--- src/main/java/com/skysoft/vaultlogic/common/domain/application/listeners/AppDomainEventsListener.java
+++ src/main/java/com/skysoft/vaultlogic/common/domain/application/listeners/AppDomainEventsListener.java
@@ -1,9 +1,8 @@
-package com.skysoft.vaultlogic.web.handler;
+package com.skysoft.vaultlogic.common.domain.application.listeners;
 
+import com.skysoft.vaultlogic.blockchain.service.BlockchainAppService;
 import com.skysoft.vaultlogic.common.domain.application.ApplicationRepository;
 import com.skysoft.vaultlogic.common.domain.application.events.AppRegistered;
-import com.skysoft.vaultlogic.web.service.JpaApplicationService;
-import com.skysoft.vaultlogic.blockchain.service.BlockchainAppService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
@@ -12,24 +11,22 @@ import org.springframework.transaction.event.TransactionalEventListener;
 
 @Slf4j
 @Component
-public class AppDomainEventsHandler implements AppEventsHandler {
+public class AppDomainEventsListener {
 
     private final ApplicationRepository applicationRepository;
-    private final JpaApplicationService jpaApplicationService;
     private final BlockchainAppService blockchainAppService;
 
     @Autowired
-    public AppDomainEventsHandler(ApplicationRepository applicationRepository, JpaApplicationService jpaApplicationService, BlockchainAppService blockchainAppService) {
+    public AppDomainEventsListener(ApplicationRepository applicationRepository, BlockchainAppService blockchainAppService) {
         this.applicationRepository = applicationRepository;
-        this.jpaApplicationService = jpaApplicationService;
         this.blockchainAppService = blockchainAppService;
     }
 
     @Async
-    @Override
     @TransactionalEventListener
     public void handleAppRegisteredEvent(AppRegistered event) {
         log.info("[x]---> Handling application registered event for app with name: {}", event.name);
+        blockchainAppService.saveApplication(applicationRepository.findSmartContractApplicationByName(event.name));
     }
 
 }
