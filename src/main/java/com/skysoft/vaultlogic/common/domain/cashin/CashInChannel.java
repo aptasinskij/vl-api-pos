05572aaa6ecdf6a4efd1a94e4beb1747e3ff44c3
Andrew Ptasinskij <aptasinskij@gmail.com>
2018-09-07 16:22:10 +0300

    expanded CashInChannel entity

--- src/main/java/com/skysoft/vaultlogic/common/domain/cashin/CashInChannel.java
+++ src/main/java/com/skysoft/vaultlogic/common/domain/cashin/CashInChannel.java
@@ -1,20 +1,38 @@
 package com.skysoft.vaultlogic.common.domain.cashin;
 
+import com.skysoft.vaultlogic.common.domain.cashin.events.CashInCloseRequested;
+import com.skysoft.vaultlogic.common.domain.cashin.events.CashInClosed;
+import com.skysoft.vaultlogic.common.domain.cashin.events.CashInCreated;
+import com.skysoft.vaultlogic.common.domain.cashin.events.CashInOpened;
 import com.skysoft.vaultlogic.common.domain.session.Session;
+import lombok.Getter;
+import org.hibernate.annotations.NaturalId;
+import org.springframework.data.domain.AbstractAggregateRoot;
 
 import javax.persistence.*;
 import java.math.BigInteger;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 
+import static java.util.Objects.requireNonNull;
+import static javax.persistence.EnumType.STRING;
 import static javax.persistence.FetchType.LAZY;
+import static javax.persistence.GenerationType.IDENTITY;
 
+@Getter
 @Entity
-public class CashInChannel {
+public class CashInChannel extends AbstractAggregateRoot<CashInChannel> {
 
     @Id
+    @GeneratedValue(strategy = IDENTITY)
     private Long id;
+
+    @NaturalId
+    @Column(nullable = false)
+    private BigInteger channelId;
 
     @JoinColumn(name = "session_id")
     @ManyToOne(fetch = LAZY, optional = false)
@@ -26,17 +44,88 @@ public class CashInChannel {
     @ElementCollection
     private Set<Split> splits = new HashSet<>();
 
+    @Enumerated(STRING)
+    @Column(nullable = false)
+    private Status status;
+
+    public CashInChannel() {
+    }
+
+    private CashInChannel(BigInteger channelId, Session session, Status status) {
+        this.channelId = channelId;
+        this.session = session;
+        this.status = status;
+    }
+
+    public static CashInChannel newChannel(BigInteger channelId, Session session, Status status) {
+        requireNonNull(channelId);
+        requireNonNull(session);
+        requireNonNull(status);
+        return new CashInChannel(channelId, session, status);
+    }
+
+    public CashInChannel emitCreated() {
+        registerEvent(CashInCreated.of(this.session.getXToken(), this.channelId));
+        return this;
+    }
+
+    public CashInChannel markOpened() {
+        this.status = Status.OPENED;
+        return andEvent(CashInOpened.of(this.channelId));
+    }
+
+    public CashInChannel markHalfClosed(String xToken) {
+        this.status = Status.HALF_CLOSED;
+        return andEvent(CashInCloseRequested.of(this.channelId, xToken));
+    }
+
+    public CashInChannel markClosed() {
+        this.status = Status.CLOSED;
+        return andEvent(CashInClosed.of(this.channelId));
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         CashInChannel that = (CashInChannel) o;
-        return Objects.equals(id, that.id);
+        return Objects.equals(channelId, that.channelId);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id);
+        return Objects.hash(channelId);
+    }
+
+    public enum Status {
+
+        PENDING(BigInteger.valueOf(0)),
+        OPENED(BigInteger.valueOf(1)),
+        HALF_CLOSED(BigInteger.valueOf(2)),
+        CLOSED(BigInteger.valueOf(3));
+
+        private final BigInteger value;
+
+        Status(BigInteger value) {
+            this.value = value;
+        }
+
+        public BigInteger getValue() {
+            return value;
+        }
+
+        public static Status from(BigInteger value) {
+            return Stream.of(values()).filter(equalTo(value)).findAny().orElseThrow(RuntimeException::new);
+        }
+
+        private static Predicate<CashInChannel.Status> equalTo(BigInteger value) {
+            return status -> status.getValue().compareTo(value) == 0;
+        }
+
     }
 
 }
