2f65368f5a931bd4a59b08a52481ef15c359ea18
Andrew Ptasinskij <aptasinskij@gmail.com>
2018-09-07 16:20:19 +0300

    enabled asynchronous support, refactored application domain events listener

--- src/main/java/com/skysoft/vaultlogic/common/listeners/ApplicationDomainEventsListener.java
+++ src/main/java/com/skysoft/vaultlogic/common/listeners/ApplicationDomainEventsListener.java
@@ -1,32 +1,37 @@
 package com.skysoft.vaultlogic.common.listeners;
 
 import com.skysoft.vaultlogic.blockchain.service.BlockchainAppService;
-import com.skysoft.vaultlogic.common.domain.application.ApplicationRepository;
 import com.skysoft.vaultlogic.common.domain.application.events.AppRegistered;
+import com.skysoft.vaultlogic.web.service.ApplicationService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.event.TransactionalEventListener;
+
+import static org.springframework.transaction.annotation.Propagation.NESTED;
 
 @Slf4j
 @Component
-public class AppDomainEventsListener {
+public class ApplicationDomainEventsListener {
 
-    private final ApplicationRepository applicationRepository;
     private final BlockchainAppService blockchainAppService;
 
+    private final ApplicationService applicationService;
+
     @Autowired
-    public AppDomainEventsListener(ApplicationRepository applicationRepository, BlockchainAppService blockchainAppService) {
-        this.applicationRepository = applicationRepository;
+    public ApplicationDomainEventsListener(BlockchainAppService blockchainAppService, ApplicationService applicationService) {
         this.blockchainAppService = blockchainAppService;
+        this.applicationService = applicationService;
     }
 
     @Async
     @TransactionalEventListener
-    public void handleAppRegisteredEvent(AppRegistered event) {
+    @Transactional(propagation = NESTED)
+    public void registered(AppRegistered event) {
         log.info("[x]---> Handling application registered event for app with name: {}", event.name);
-        blockchainAppService.saveApplication(applicationRepository.findSmartContractApplicationByName(event.name));
+        blockchainAppService.saveApplication(applicationService.findSmartContractApplicationByName(event.name));
     }
 
 }
