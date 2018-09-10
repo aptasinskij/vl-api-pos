4046b32b7bf45c14706b79a8bf00b09f2b0dd843
Andrew Ptasinskij <aptasinskij@gmail.com>
2018-09-10 14:36:46 +0300

    refactored cash insert action handler to call cash in channel service

--- src/main/java/com/skysoft/vaultlogic/web/postback/impl/handlers/CashInsertActionHandler.java
+++ src/main/java/com/skysoft/vaultlogic/web/postback/impl/handlers/CashInsertActionHandler.java
@@ -5,10 +5,10 @@ import com.skysoft.vaultlogic.web.postback.api.AbstractEventHandler;
 import com.skysoft.vaultlogic.web.postback.api.EventEmptyResponse;
 import com.skysoft.vaultlogic.web.postback.impl.factories.markers.CashActionHandler;
 import com.skysoft.vaultlogic.web.postback.impl.protocol.data.CashInsert;
+import com.skysoft.vaultlogic.web.service.CashInService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.web3j.protocol.core.methods.response.TransactionReceipt;
 
 @Slf4j
 @Component
@@ -16,28 +16,19 @@ public class CashInsertActionHandler extends AbstractEventHandler<CashInsert, Ev
 
     private static final String CASH_INSERT_HANDLER = "insert";
 
+    private final CashInService cashInService;
+
     @Autowired
-    protected CashInsertActionHandler(DataMapper dataMapper) {
+    protected CashInsertActionHandler(DataMapper dataMapper, CashInService cashInService) {
         super(CashInsert.class, dataMapper);
+        this.cashInService = cashInService;
     }
 
     @Override
     protected EventEmptyResponse handleEvent(CashInsert eventData, String xToken) {
         log.info("[x] ---> More money inserted: {} for session: {}", eventData, xToken);
-//        vaultLogic.addMoneyToChannel(xToken, eventData.getCurrentAmount().toBigInteger())
-//                .sendAsync()
-//                .thenAccept(this::moneyAddedSuccessfully)
-//                .exceptionally(this::failedToAddMoney);
+        cashInService.updateBalance(eventData, xToken);
         return new EventEmptyResponse();
-    }
-
-    private void moneyAddedSuccessfully(TransactionReceipt transactionReceipt) {
-        log.info("[x] ---> Succeed in adding money to channel! TxHash: {}", transactionReceipt.getTransactionHash());
-    }
-
-    private Void failedToAddMoney(Throwable throwable) {
-        log.error("[x] ---> Failed in adding money to channel! Reason: {}", throwable.getMessage(), throwable);
-        return null;
     }
 
     @Override
