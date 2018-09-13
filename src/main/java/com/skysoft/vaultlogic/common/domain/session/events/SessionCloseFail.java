1dd25cb749355be553016df57740784488203393
ricklestatdt <RickLestatDT@gmail.com>
2018-09-13 17:00:10 +0300

    Added new session statuses, new methods to emit event with new statuses and set new status for session.

--- src/main/java/com/skysoft/vaultlogic/common/domain/session/events/SessionCloseFail.java
+++ src/main/java/com/skysoft/vaultlogic/common/domain/session/events/SessionCloseFail.java
@@ -3,8 +3,8 @@ package com.skysoft.vaultlogic.common.domain.session.events;
 import lombok.Value;
 
 @Value(staticConstructor = "of")
-public class SessionCloseRequested {
+public class SessionCloseFail {
 
-    String xToken;
+    public String xToken;
 
 }
