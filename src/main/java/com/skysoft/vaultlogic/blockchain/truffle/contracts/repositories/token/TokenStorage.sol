09ca07173fd85d5a4c2b398f047424e95f60c3f7
Imant Kukhar <imantkuhar93@gmail.com>
2018-09-17 19:00:00 +0300

    added token storage interface and realisation

---
+++ src/main/java/com/skysoft/vaultlogic/blockchain/truffle/contracts/repositories/token/TokenStorage.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.4.0;
+
+import "./TokenLib.sol";
+import "../../registry/RegistryComponent.sol";
+
+contract TokenStorage is RegistryComponent {
+
+    string constant COMPONENT_NAME = "token-storage";
+    string constant DATABASE = "database";
+
+    using TokenLib for address;
+
+    constructor(address registryAddress) RegistryComponent(registryAddress) public {}
+
+    function getName() internal pure returns(string) {
+        return COMPONENT_NAME;
+    }
+
+    function set(address customer, uint256 amount) public {
+        return lookup(DATABASE).set(customer, amount);
+    }
+
+    function get(address consumer) public view returns (uint256) {
+        return lookup(DATABASE).get(consumer);
+    }
+
+}
