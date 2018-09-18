4b46353f7174974ef2cc62be66a4dba407a481b3
Imant Kukhar <imantkuhar93@gmail.com>
2018-09-18 12:04:57 +0300

    added SafeMath library and new TransferFrom event

--- src/main/java/com/skysoft/vaultlogic/blockchain/truffle/contracts/services/TokenManager.sol
+++ src/main/java/com/skysoft/vaultlogic/blockchain/truffle/contracts/services/TokenManager.sol
@@ -1,53 +1,55 @@
-pragma solidity ^0.4.0;
+pragma solidity 0.4.24;
 
+import "../library/SafeMath.sol";
 import "../registry/RegistryComponent.sol";
 import "../repositories/token/ITokenStorage.sol";
 
 contract TokenManager is RegistryComponent {
 
     string constant COMPONENT_NAME = "token-manager";
-
     string constant TOKEN_STORAGE = "token-storage";
 
-    constructor(address registryAddress) RegistryComponent(registryAddress) public {}
+    event Transfer(address indexed to, uint value);
+    event TransferFrom(address indexed from, address indexed to, uint value);
 
-    modifier onlyPayloadSize(uint size) {
-        assert(msg.data.length == size + 4);
-        _;
-    }
+    using SafeMath for uint256;
+
+    constructor(address registryAddress) RegistryComponent(registryAddress) public {}
 
     function getName() internal pure returns(string) {
         return COMPONENT_NAME;
     }
 
-    function balanceOf(address consumer) constant returns (uint balance) {
-        uint256 balance = ITokenStorage(lookup(TOKEN_STORAGE)).get(consumer);
+    function balanceOf(address consumer) view returns (uint) {
+        return ITokenStorage(lookup(TOKEN_STORAGE)).get(consumer);
     }
 
-    function transfer(address recipient, uint value) onlyPayloadSize(2*32) {
-        uint256 recipientAmount = ITokenStorage(lookup(TOKEN_STORAGE)).get(recipient);
-        recipientAmount += value;
-        ITokenStorage(lookup(TOKEN_STORAGE)).set(recipient, recipientAmount);
+    function transfer(address recipient, uint value) {
+        ITokenStorage tokenStorage = ITokenStorage(lookup(TOKEN_STORAGE));
 
-        emit Transfer(msg.sender, recipient, value);
+        uint256 recipientAmount = tokenStorage.get(recipient);
+        recipientAmount = recipientAmount.add(value);
+        tokenStorage.set(recipient, recipientAmount);
+
+        emit Transfer(recipient, value);
     }
 
     function transferFrom(address from, address to, uint value) {
-        uint256 balanceFrom = ITokenStorage(lookup(TOKEN_STORAGE)).get(from);
-        uint256 balanceTo = ITokenStorage(lookup(TOKEN_STORAGE)).get(to);
+        ITokenStorage tokenStorage = ITokenStorage(lookup(TOKEN_STORAGE));
+
+        uint256 balanceFrom = tokenStorage.get(from);
+        uint256 balanceTo = tokenStorage.get(to);
         require(balanceFrom >= value && value > 0);
 
-        uint256 amountFrom = ITokenStorage(lookup(TOKEN_STORAGE)).get(from);
-        amountFrom -= value;
-        ITokenStorage(lookup(TOKEN_STORAGE)).set(from, amountFrom);
+        uint256 amountFrom = tokenStorage.get(from);
+        amountFrom = amountFrom.sub(value);
+        tokenStorage.set(from, amountFrom);
 
-        uint256 amountTo = ITokenStorage(lookup(TOKEN_STORAGE)).get(to);
-        amountTo += value;
-        ITokenStorage(lookup(TOKEN_STORAGE)).set(to, amountTo);
+        uint256 amountTo = tokenStorage.get(to);
+        amountTo = amountTo.add(value);
+        tokenStorage.set(to, amountTo);
 
-        emit Transfer(from, to, value);
+        emit TransferFrom(from, to, value);
     }
-
-    event Transfer(address indexed from, address indexed to, uint value);
 
 }
