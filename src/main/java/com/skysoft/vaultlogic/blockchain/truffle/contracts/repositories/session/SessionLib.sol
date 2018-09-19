5a69ff67d94752d006864f659d94c3a3605cd41a
Andrew Ptasinskij <aptasinskij@gmail.com>
2018-09-19 15:34:20 +0300

    expanded session storage and library to be able to set has-active-cash-in flag

--- src/main/java/com/skysoft/vaultlogic/blockchain/truffle/contracts/repositories/session/SessionLib.sol
+++ src/main/java/com/skysoft/vaultlogic/blockchain/truffle/contracts/repositories/session/SessionLib.sol
@@ -7,6 +7,7 @@ library SessionLib {
     string constant APP_ID = "session_application_id";
     string constant X_TOKEN = "session_x_token";
     string constant STATUS = "session_status";
+    string constant HAS_ACTIVE_CASH_IN = "session_has_active_cash_in";
 
     function save(address self, uint256 sessionId, uint256 appId, string xToken, uint256 status) internal {
         Database(self).setUintValue(keccak256(abi.encodePacked(APP_ID, sessionId)), appId);
@@ -18,6 +19,14 @@ library SessionLib {
         appId = Database(self).getUintValue(keccak256(abi.encodePacked(APP_ID, index)));
         xToken = Database(self).getStringValue(keccak256(abi.encodePacked(X_TOKEN, index)));
         status = Database(self).getUintValue(keccak256(abi.encodePacked(STATUS, index)));
+    }
+
+    function setHasActiveCashIn(address _self, uint256 _sessionId, bool _flag) internal {
+        Database(_self).setBooleanValue(keccak256(abi.encodePacked(HAS_ACTIVE_CASH_IN, _sessionId)), _flag);
+    }
+
+    function getIsHasActiveCashIn(address _self, uint256 _sessionId) internal view returns(bool) {
+        return Database(_self).getBooleanValue(keccak256(abi.encodePacked(HAS_ACTIVE_CASH_IN, _sessionId)));
     }
 
     function getAppId(address self, uint256 index) internal view returns (uint256) {
