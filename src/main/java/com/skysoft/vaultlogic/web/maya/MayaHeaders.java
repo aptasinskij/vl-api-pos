ebaef8571545c4a672ff5c74d505d5affad46fce
Imant Kukhar <imantkuhar93@gmail.com>
2018-10-02 11:52:52 +0300

    added class for maintenance x_token and x_nonce headers

---
+++ src/main/java/com/skysoft/vaultlogic/web/maya/MayaHeaders.java
@@ -0,0 +1,21 @@
+package com.skysoft.vaultlogic.web.maya;
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+public class MayaHeaders {
+
+    private static final String X_TOKEN_HEADER = "X-Token";
+    private static final String X_NONCE_HEADER = "X-Nonce";
+
+    public static String getxTokenHeader() {
+        return X_TOKEN_HEADER;
+    }
+
+    public static String getxNonceHeader() {
+        return X_NONCE_HEADER;
+    }
+
+}
