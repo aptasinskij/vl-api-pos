10e3efb3de123952f98f796ccf5ed1a0886efb9b
Imant Kukhar <imantkuhar93@gmail.com>
2018-10-05 15:08:41 +0300

    added needed annotation in Receipt class

--- back-end/src/main/java/com/skysoft/vaultlogic/clients/api/model/Receipt.java
+++ back-end/src/main/java/com/skysoft/vaultlogic/clients/api/model/Receipt.java
@@ -1,12 +1,16 @@
 package com.skysoft.vaultlogic.clients.api.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class Receipt {
 
     private String id;
+
     private String data;
+
+    @JsonProperty("wkhtmltopdf_params")
     private String params;
 
 }
