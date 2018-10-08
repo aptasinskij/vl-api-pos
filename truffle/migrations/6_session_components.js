870bbbf8d65b79e487064ffeb9f169fb3d716712
Andrew Ptasinskij <aptasinskij@gmail.com>
2018-10-08 15:48:39 +0300

    added missed deployment for SessionController

--- truffle/migrations/6_session_components.js
+++ truffle/migrations/6_session_components.js
@@ -3,11 +3,14 @@ var SessionLib = artifacts.require("./SessionLib.sol");
 var SessionStorage = artifacts.require("./SessionStorage.sol");
 var SessionManager = artifacts.require("./SessionManager.sol");
 var SessionOracle = artifacts.require("./SessionOracle.sol");
+var SessionController = artifacts.require("SessionController");
 
 module.exports = function (deployer) {
     deployer.deploy(SessionLib);
     deployer.link(SessionLib, SessionStorage);
     deployer.link(SessionLib, SessionManager);
+    deployer.link(SessionLib, SessionController);
+    deployer.deploy(SessionController, ContractRegistry.address);
     deployer.deploy(SessionStorage, ContractRegistry.address);
     deployer.deploy(SessionManager, ContractRegistry.address);
     deployer.deploy(SessionOracle, ContractRegistry.address);
