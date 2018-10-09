92ed8bbe829849623d1f9128e13196aa2db9a443
Andrew Ptasinskij <aptasinskij@gmail.com>
2018-10-09 11:49:11 +0300

    return redirect view rather actual html payload

--- back-end/src/main/java/com/skysoft/vaultlogic/controllers/local/LocalApplicationIndexController.java
+++ back-end/src/main/java/com/skysoft/vaultlogic/controllers/local/LocalApplicationIndexController.java
@@ -3,54 +3,33 @@ package com.skysoft.vaultlogic.controllers.local;
 import com.skysoft.vaultlogic.common.domain.session.Session;
 import com.skysoft.vaultlogic.services.ApplicationService;
 import com.skysoft.vaultlogic.services.SessionService;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
-import org.springframework.http.RequestEntity;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.math.BigInteger;
 import java.net.URI;
 
-import static org.springframework.http.MediaType.TEXT_HTML;
+import static java.lang.String.format;
 
 @Slf4j
 @RestController
 @Profile("ganache")
+@AllArgsConstructor
 @RequestMapping("/index/{appId}")
 public class LocalApplicationIndexController {
 
-    private static final String VL_TOKEN = "Vault-Logic-Token";
-
     private final ApplicationService applicationService;
     private final SessionService sessionService;
-    private final RestTemplate restTemplate;
-
-    @Autowired
-    public LocalApplicationIndexController(ApplicationService applicationService,
-                                           SessionService sessionService,
-                                           RestTemplate restTemplate) {
-        this.applicationService = applicationService;
-        this.sessionService = sessionService;
-        this.restTemplate = restTemplate;
-    }
 
     @GetMapping
-    public ResponseEntity<String> getApplicationIndexPage(@PathVariable BigInteger appId, @RequestParam("token") String xToken) {
+    public RedirectView getApplicationIndexPage(@PathVariable BigInteger appId, @RequestParam("token") String xToken) {
         URI appIndexUri = applicationService.getApplicationUri(appId);
         Session session = sessionService.createApplicationSession(appId, xToken);
-        RequestEntity<Void> request = buildRequest(appIndexUri, String.valueOf(session.getId()));
         sessionService.activate(session.getId());
-        return restTemplate.exchange(request, String.class);
-    }
-
-    private RequestEntity<Void> buildRequest(URI appIndexUri, String token) {
-        return RequestEntity.get(appIndexUri)
-                .accept(TEXT_HTML)
-                .header(VL_TOKEN, token)
-                .build();
+        return new RedirectView(format("%s?token=%d", appIndexUri.toString(), session.getId()));
     }
 
 }
