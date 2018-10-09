862ceee015ac2f71968c08893f95da7dfec05d46
Andrew Ptasinskij <aptasinskij@gmail.com>
2018-10-09 16:50:53 +0300

    clean up prod app index controller

--- back-end/src/main/java/com/skysoft/vaultlogic/controllers/cloud/CloudApplicationIndexController.java
+++ back-end/src/main/java/com/skysoft/vaultlogic/controllers/cloud/CloudApplicationIndexController.java
@@ -1,85 +1,30 @@
 package com.skysoft.vaultlogic.controllers.cloud;
 
-import com.skysoft.vaultlogic.common.domain.session.Session;
-import com.skysoft.vaultlogic.services.ApplicationService;
 import com.skysoft.vaultlogic.services.SessionService;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
-import org.springframework.http.MediaType;
-import org.springframework.http.RequestEntity;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.data.util.Pair;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.math.BigInteger;
-import java.net.URI;
 
-import static java.lang.String.valueOf;
-import static java.lang.System.currentTimeMillis;
+import static java.lang.String.format;
 
 @Slf4j
 @RestController
+@AllArgsConstructor
 @Profile("cloud-quorum")
 @RequestMapping("/index/{appId}")
 public class CloudApplicationIndexController {
 
-    private static final String VL_TOKEN = "Vault-Logic-Token";
-
-    private static final String LAUNCH_APPLICATION_URL = "https://api-staging.maya.tech/open/application/launch";
-    private static final String X_TOKEN = "X-Token";
-    private static final String X_NONCE = "x-Nonce";
-
     private final SessionService sessionService;
-    private final ApplicationService applicationService;
-    private final OAuth2RestTemplate oAuth2RestTemplate;
-
-    @Autowired
-    public CloudApplicationIndexController(SessionService sessionService, ApplicationService applicationService, OAuth2RestTemplate oAuth2RestTemplate) {
-        this.sessionService = sessionService;
-        this.applicationService = applicationService;
-        this.oAuth2RestTemplate = oAuth2RestTemplate;
-    }
 
     @GetMapping
-    public ResponseEntity<String> getApplicationIndexPage(@PathVariable BigInteger appId, @RequestParam("token") String xToken) {
-        URI appIndexUri = applicationService.getApplicationUri(appId);
-        Session session = sessionService.createApplicationSession(appId, xToken);
-        ResponseEntity<String> responseEntity = getIndexPage(appIndexUri, session.getId().longValue());
-        launchApplication(xToken);
-        return responseEntity;
-    }
-
-    private ResponseEntity<String> getIndexPage(URI appIndexUri, Long sessionId) {
-        try {
-            return oAuth2RestTemplate.exchange(getApplicationIndexRequestEntity(appIndexUri, sessionId), String.class);
-        } catch (Exception e) {
-            log.error("[x] ---> Failed to get application index page. Reason: {}", e.getMessage(), e);
-            throw new RuntimeException(e);
-        }
-    }
-
-    private ResponseEntity<String> launchApplication(String xToken) {
-        try {
-            return oAuth2RestTemplate.exchange(getLaunchApplicationRequestEntity(xToken), String.class);
-        } catch (Exception e) {
-            log.error("[x] ---> Failed to launch application. Reason: {}", e.getMessage(), e);
-            throw new RuntimeException(e);
-        }
-    }
-
-    private RequestEntity<Void> getApplicationIndexRequestEntity(URI appIndexUri, Long sessionId) {
-        return RequestEntity.get(appIndexUri)
-                .accept(MediaType.TEXT_HTML)
-                .header(VL_TOKEN, valueOf(sessionId))
-                .build();
-    }
-
-    private RequestEntity<Void> getLaunchApplicationRequestEntity(String xToken) {
-        return RequestEntity.post(URI.create(LAUNCH_APPLICATION_URL))
-                .header(X_TOKEN, xToken)
-                .header(X_NONCE, valueOf(currentTimeMillis()))
-                .build();
+    public RedirectView getApplicationIndexPage(@PathVariable BigInteger appId, @RequestParam("token") String xToken) {
+        Pair<String, BigInteger> appUrlSessionId = sessionService.createSession(appId, xToken);
+        return new RedirectView(format("%s?token=%d", appUrlSessionId.getFirst(), appUrlSessionId.getSecond()));
     }
 
 }
