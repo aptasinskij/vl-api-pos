550f6d88d83df7055f2ff35e01ad9c8ba8c8d145
Andrew Ptasinskij <aptasinskij@gmail.com>
2018-10-10 15:31:44 +0300

    test cases: kiosk not resolved; application not exists in db; positive;

--- back-end/src/test/java/com/skysoft/vaultlogic/services/quorum/QuorumSessionServiceTest.java
+++ back-end/src/test/java/com/skysoft/vaultlogic/services/quorum/QuorumSessionServiceTest.java
@@ -1,27 +1,34 @@
 package com.skysoft.vaultlogic.services.quorum;
 
 import com.skysoft.vaultlogic.clients.api.KioskApplicationClient;
+import com.skysoft.vaultlogic.clients.api.model.StatusCode;
+import com.skysoft.vaultlogic.common.domain.application.Application;
 import com.skysoft.vaultlogic.common.domain.application.ApplicationRepository;
+import com.skysoft.vaultlogic.common.domain.kiosk.Kiosk;
 import com.skysoft.vaultlogic.common.domain.session.Session;
 import com.skysoft.vaultlogic.common.domain.session.SessionRepository;
+import com.skysoft.vaultlogic.common.domain.user.User;
 import com.skysoft.vaultlogic.services.KioskService;
 import io.vavr.control.Try;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.util.Pair;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.math.BigInteger;
+import java.util.Optional;
+import java.util.UUID;
 
+import static java.math.BigInteger.ZERO;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.charThat;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @RunWith(SpringRunner.class)
 public class QuorumSessionServiceTest {
@@ -66,10 +73,69 @@ public class QuorumSessionServiceTest {
     @Test(expected = RuntimeException.class)
     public void givenFailedToLaunchApplication_whenError_thanCorrect() {
         when(kioskApplicationClient.launchApplication(anyString())).thenReturn(Try.failure(new RuntimeException()));
-        quorumSessionService.createSession(BigInteger.ZERO, "xToken");
+        quorumSessionService.createSession(ZERO, "xToken");
         verify(kioskService, never()).resolveKioskForSession(anyString());
         verify(applicationRepository, never()).findById(any(BigInteger.class));
         verify(sessionRepository, never()).save(any(Session.class));
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void givenKioskNotFound_whenError_thanCorrect() {
+        StatusCode statusCode = new StatusCode();
+        when(kioskApplicationClient.launchApplication(anyString())).thenReturn(Try.success(statusCode));
+        when(kioskService.resolveKioskForSession(anyString())).thenReturn(Optional.empty());
+        quorumSessionService.createSession(ZERO, "xToken");
+        verify(kioskApplicationClient, times(1)).launchApplication(anyString());
+        verify(kioskService, timeout(1)).resolveKioskForSession(anyString());
+        verify(sessionRepository, never()).save(any(Session.class));
+        verify(applicationRepository, never()).findById(any(BigInteger.class));
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void givenKioskApplicationLaunchedKioskResolvedApplicationNotFound_whenError_thanCorrect() {
+        BigInteger applicationId = BigInteger.ONE;
+        String xToken = UUID.randomUUID().toString();
+        StatusCode statusCode = new StatusCode();
+        Kiosk kiosk = Kiosk.kiosk("shortId", "address", "name", "timezone");
+
+        when(kioskApplicationClient.launchApplication(xToken)).thenReturn(Try.success(statusCode));
+        when(kioskService.resolveKioskForSession(xToken)).thenReturn(Optional.of(kiosk));
+        when(applicationRepository.findById(applicationId)).thenReturn(Optional.empty());
+
+        quorumSessionService.createSession(applicationId, xToken);
+
+        verify(kioskApplicationClient, times(1)).launchApplication(xToken);
+        verify(kioskService, times(1)).resolveKioskForSession(xToken);
+        verify(applicationRepository, times(1)).findById(applicationId);
+        verify(sessionRepository, never()).save(any(Session.class));
+    }
+
+    @Test
+    public void givenApplicationLaunchedKioskResolved_whenSuccess_thanCorrect() {
+        StatusCode statusCode = new StatusCode();
+        BigInteger applicationId = BigInteger.ONE;
+        BigInteger sessionId = BigInteger.valueOf(1L);
+        String xToken = UUID.randomUUID().toString();
+        Kiosk kiosk = Kiosk.kiosk("shortId", "address", "name", "timezone");
+        User user = User.newUser("username", "secret", "0x0");
+        Application application = Application.newApplication("test", "http", user, "0x0");
+        Session session = Mockito.mock(Session.class);
+        when(session.getId()).thenReturn(sessionId);
+        when(session.getApplication()).thenReturn(application);
+        when(kioskApplicationClient.launchApplication(anyString())).thenReturn(Try.success(statusCode));
+        when(kioskService.resolveKioskForSession(anyString())).thenReturn(Optional.of(kiosk));
+        when(applicationRepository.findById(applicationId)).thenReturn(Optional.of(application));
+        when(sessionRepository.save(any(Session.class))).thenReturn(session);
+
+        Pair<String, BigInteger> stringBigIntegerPair = quorumSessionService.createSession(applicationId, xToken);
+
+        verify(kioskApplicationClient, times(1)).launchApplication(xToken);
+        verify(kioskService, times(1)).resolveKioskForSession(xToken);
+        verify(applicationRepository, times(1)).findById(applicationId);
+        verify(sessionRepository, times(1)).save(any(Session.class));
+
+        assertEquals(application.getUri(), stringBigIntegerPair.getFirst());
+        assertEquals(session.getId(), stringBigIntegerPair.getSecond());
     }
 
 }
