0ea3f91fcb250140dce27cf3b1eced15aa716e56
Andrew Ptasinskij <aptasinskij@gmail.com>
2018-10-12 19:05:20 +0300

    CameraOracle onNext methods returns bool

--- truffle/contracts/oracles/Oracles.sol
+++ truffle/contracts/oracles/Oracles.sol
@@ -5,7 +5,7 @@ contract ACameraOracle {
     event StartScanQR(uint256 _commandId, uint256 _sessionId, bool _lights);
     event StopScanQR(uint256 _commandId, uint256 _sessionId);
 
-    function onNextStartQRScan(uint256 _commandId) public;
+    function onNextStartQRScan(uint256 _commandId) public returns (bool _accepted);
 
     function successStart(uint256 _commandId) public;
 
@@ -13,7 +13,7 @@ contract ACameraOracle {
 
     function failQRScan(uint256 _commandId) public;
 
-    function onNextStopQRScan(uint256 _commandId) public;
+    function onNextStopQRScan(uint256 _commandId) public returns (bool _accepted);
 
     function successStopQRScan(uint256 _commandId) public;
 
