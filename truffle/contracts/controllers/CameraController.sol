06cb8ec6a1f9310c61f880fefc80df320ecce165
Andrew Ptasinskij <aptasinskij@gmail.com>
2018-10-12 17:19:58 +0300

    extended ACameraController for implementation

---
+++ truffle/contracts/controllers/CameraController.sol
@@ -0,0 +1,54 @@
+pragma solidity 0.4.24;
+
+import {ACameraController} from "./Controllers.sol";
+
+contract CameraController is ACameraController {
+
+    string constant COMPONENT_NAME = "camera-controller";
+
+    constructor(address registry) ACameraController(registry) public {}
+
+    function getName() internal pure returns (string name) {
+        return COMPONENT_NAME;
+    }
+
+    function scanQRCodeWithLights(
+        uint256 _sessionId,
+        function(uint256, string memory, string memory, string memory) external _success,
+        function(uint256) external fail
+    )   // @formatter:off
+        public
+        onlyRegisteredApp
+        returns (bool _accepted)
+        // @formatter:on
+    {
+
+    }
+
+    function scanQRCode(
+        uint256 _sessionId,
+        function(uint256, string memory, string memory, string memory) external _success,
+        function(uint256) external fail
+    )   // @formatter:off
+        public
+        onlyRegisteredApp
+        returns (bool _accepted)
+        // @formatter:on
+    {
+
+    }
+
+    function stopQRScanning(
+        uint256 _sessionId,
+        function(uint256) external success,
+        function(uint256) external fail
+    )   // @formatter:off
+        public
+        onlyRegisteredApp
+        returns (bool _accepted)
+        // @formatter:on
+    {
+
+    }
+
+}
