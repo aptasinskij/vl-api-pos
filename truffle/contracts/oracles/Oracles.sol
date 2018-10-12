927ca3cd26eb421eed3d4737c752f9c5f652f86c
Andrew Ptasinskij <aptasinskij@gmail.com>
2018-10-12 17:53:23 +0300

    definition of camera oracle

---
+++ truffle/contracts/oracles/Oracles.sol
@@ -0,0 +1,22 @@
+pragma solidity 0.4.24;
+
+contract ACameraOracle {
+
+    event StartScanQR(uint256 _commandId, uint256 _sessionId, bool _lights);
+    event StopScanQR(uint256 _commandId, uint256 _sessionId);
+
+    function onNextStartQRScan(uint256 _commandId) public;
+
+    function successStart(uint256 _commandId) public;
+
+    function qrScanned(uint256 _sessionId, string memory _port, string memory _url, string memory _href) public;
+
+    function failQRScan(uint256 _commandId) public;
+
+    function onNextStopQRScan(uint256 _commandId) public;
+
+    function successStopQRScan(uint256 _commandId) public;
+
+    function failStopQRScan(uint256 _commandId) public;
+
+}
