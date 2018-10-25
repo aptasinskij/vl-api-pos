f76d9a757bdeeda1f62e4ba684f458395ff7007f
Andrew Ptasinskij <aptasinskij@gmail.com>
2018-10-25 10:20:21 +0300

    clients returns Try rather Either

--- back-end/src/main/java/com/skysoft/vaultlogic/clients/impl/KioskCashDevicesHttpClient.java
+++ back-end/src/main/java/com/skysoft/vaultlogic/clients/impl/KioskCashDevicesHttpClient.java
@@ -4,15 +4,12 @@ import com.skysoft.vaultlogic.clients.api.KioskCashDevices;
 import com.skysoft.vaultlogic.clients.api.model.*;
 import com.skysoft.vaultlogic.common.configuration.properties.MayaProperties;
 import io.vavr.control.Either;
+import io.vavr.control.Try;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpEntity;
-import org.springframework.http.RequestEntity;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.stereotype.Service;
-
-import java.util.function.Function;
 
 import static com.skysoft.vaultlogic.clients.RequestFactory.post;
 import static io.vavr.API.Try;
@@ -25,42 +22,36 @@ public class KioskCashDevicesHttpClient implements KioskCashDevices {
     private final MayaProperties maya;
     private final OAuth2RestTemplate rest;
 
-    private <T> ResponseEntity<T> exchange(RequestEntity<?> request, Class<T> responseType) {
-        return rest.exchange(request, responseType);
-    }
-
     @Override
     public Either<Throwable, CashDevicesStatus> getStatus(String xToken) {
-        return Try(() -> exchange(post(xToken, maya::cashDeviceStatusURI), CashDevicesStatus.class))
-                .map(getBody())
+        return Try(() -> rest.exchange(post(xToken, maya::cashDeviceStatusURI), CashDevicesStatus.class))
+                .map(HttpEntity::getBody)
                 .toEither();
     }
 
     @Override
     public Either<Throwable, RecyclerStatus> getRecyclerStatus(String xToken) {
-        return Try(() -> exchange(post(xToken, maya::recyclerDeviceStatusURI), RecyclerStatus.class))
-                .map(getBody())
+        return Try(() -> rest.exchange(post(xToken, maya::recyclerDeviceStatusURI), RecyclerStatus.class))
+                .map(HttpEntity::getBody)
                 .toEither();
     }
 
     @Override
-    public Either<Throwable, CashAcceptorStatus> enableCashAcceptor(String xToken, CashAcceptorConfig cashAcceptorConfig) {
-        return Try(() -> exchange(post(xToken, maya::enableCashAcceptorURI, cashAcceptorConfig), CashAcceptorStatus.class))
-                .map(getBody())
-                .toEither();
+    public Try<CashAcceptorStatus> enableCashAcceptor(String xToken, CashAcceptorConfig cashAcceptorConfig) {
+        return Try(() -> rest.exchange(post(xToken, maya::enableCashAcceptorURI, cashAcceptorConfig), CashAcceptorStatus.class))
+                .map(HttpEntity::getBody);
     }
 
     @Override
-    public Either<Throwable, CashAcceptorStatus> disableCashAcceptor(String xToken) {
-        return Try(() -> exchange(post(xToken, maya::disableCashAcceptorURI), CashAcceptorStatus.class))
-                .map(getBody())
-                .toEither();
+    public Try<CashAcceptorStatus> disableCashAcceptor(String xToken) {
+        return Try(() -> rest.exchange(post(xToken, maya::disableCashAcceptorURI), CashAcceptorStatus.class))
+                .map(HttpEntity::getBody);
     }
 
     @Override
     public Either<Throwable, DispensableAmount> getDispensableAmount(String xToken, DispensableAmount amountToDispense) {
-        return Try(() -> exchange(post(xToken, maya::dispensableAmountURI, amountToDispense), DispensableAmount.class))
-                .map(getBody())
+        return Try(() -> rest.exchange(post(xToken, maya::dispensableAmountURI, amountToDispense), DispensableAmount.class))
+                .map(HttpEntity::getBody)
                 .toEither();
     }
 
@@ -69,10 +60,6 @@ public class KioskCashDevicesHttpClient implements KioskCashDevices {
         return Try(() -> rest.exchange(post(xToken, maya::dispenseCashURI, dispenseCash), StatusCode.class))
                 .map(HttpEntity::getBody)
                 .toEither();
-    }
-
-    private <T> Function<ResponseEntity<T>, T> getBody() {
-        return ResponseEntity::getBody;
     }
 
 }
