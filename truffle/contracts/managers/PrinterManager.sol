f6b44901e28b2d1cebc6506f7a512ae2434f512e
Imant Kukhar <imantkuhar93@gmail.com>
2018-11-05 16:40:06 +0200

    refactored printer manager api and it realization

--- truffle/contracts/managers/PrinterManager.sol
+++ truffle/contracts/managers/PrinterManager.sol
@@ -5,18 +5,18 @@ import "../platform/Mortal.sol";
 import "../platform/Component.sol";
 import "./api/APrinterManager.sol";
 import "../oracles/api/APrinterOracle.sol";
+import "../storages/api/APrinterStorage.sol";
 import "../controllers/api/APrinterController.sol";
-import {PrinterLib} from "../libs/Libraries.sol";
 
 contract PrinterManager is APrinterManager, Mortal, Named("printer-manager"), Component {
 
-    using PrinterLib for address;
-
+    string constant STORAGE = "printer-storage";
     string constant ORACLE = "printer-oracle";
     string constant CONTROLLER = "printer-controller";
 
     constructor(address _config) Component(_config) public {}
 
+    // @formatter:off
     function createReceipt(
         address _application,
         uint256 _sessionId,
@@ -24,71 +24,48 @@ contract PrinterManager is APrinterManager, Mortal, Named("printer-manager"), Co
         function(uint256) external _fail
     )
         public
-        returns (bool _accepted)
+    // @formatter:on
     {
-        /*address sessionOwner = database.retrieveSessionApplicationDeployedAddress(_sessionId);
-        require(sessionOwner == _application, "illegal session access");
-        require(database.sessionIsActive(_sessionId), "session is not active");*/
-        uint256 receiptCreateId = database.getNextReceiptCreateId();
-        // @formatter:off
-        database.createReceiptCreate(PrinterLib.ReceiptCreate({
-            id: receiptCreateId,
-            sessionId: _sessionId,
-            success: _success,
-            fail: _fail
-        }));
-        // @formatter:on
-        return APrinterOracle(context.get(ORACLE)).onNextReceiptCreate(receiptCreateId);
+        uint256 _id = APrinterStorage(context.get(STORAGE)).createReceiptCreate(_sessionId, _success, _fail);
+        APrinterOracle(context.get(ORACLE)).onNextReceiptCreate(_id, _sessionId);
     }
 
-    function confirmCreate(uint256 _commandId, string memory _receiptId, string memory _url) public {
-        PrinterLib.ReceiptCreate memory command = database.retrieveReceiptCreate(_commandId);
-        APrinterController(context.get(CONTROLLER)).respondCreate(command.sessionId, _receiptId, _url, command.success);
+    function confirmCreate(uint256 _commandId, string _receiptId, string _url) public {
+        (uint256 _sessionId,function(uint256, string memory, string memory) external callback,) = APrinterStorage(context.get(STORAGE)).
+        getReceiptCreate(_commandId);
+        APrinterController(context.get(CONTROLLER)).respondCreate(_sessionId, _receiptId, _url, callback);
     }
 
     function confirmFailCreate(uint256 _commandId) public {
-        PrinterLib.ReceiptCreate memory command = database.retrieveReceiptCreate(_commandId);
-        APrinterController(context.get(CONTROLLER)).respondFailCreate(command.sessionId, command.fail);
+        (uint256 _sessionId,,function(uint256) external callback) = APrinterStorage(context.get(STORAGE)).getReceiptCreate(_commandId);
+        APrinterController(context.get(CONTROLLER)).respondFailCreate(_sessionId, callback);
     }
 
+    // @formatter:off
     function printReceipt(
         address _application,
         uint256 _sessionId,
-        string memory _receiptId,
-        string memory _data,
-        string memory _params,
+        string _receiptId,
+        string _data,
+        string _params,
         function(uint256) external _success,
         function(uint256) external _fail
     )
-        public
-        returns (bool _accepted)
+    public
+    // @formatter:on
     {
-        /*address sessionOwner = database.retrieveSessionApplicationDeployedAddress(_sessionId);
-        require(sessionOwner == _application, "illegal session access");
-        require(database.sessionIsActive(_sessionId), "session is not active");*/
-        uint256 receiptPrintId = database.getNextReceiptPrintId();
-        // @formatter:off
-        database.createReceiptPrint(PrinterLib.ReceiptPrint({
-            id: receiptPrintId,
-            sessionId: _sessionId,
-            receiptId: _receiptId,
-            data: _data,
-            params: _params,
-            success: _success,
-            fail: _fail
-        }));
-        // @formatter:on
-        return APrinterOracle(context.get(ORACLE)).onNextReceiptPrint(receiptPrintId);
+        uint256 _id = APrinterStorage(context.get(STORAGE)).createReceiptPrint(_sessionId, _receiptId, _data, _params, _success, _fail);
+        APrinterOracle(context.get(ORACLE)).onNextReceiptPrint(_id, _sessionId, _receiptId, _data, _params);
     }
 
     function confirmPrint(uint256 _commandId) public {
-        PrinterLib.ReceiptPrint memory command = database.retrieveReceiptPrint(_commandId);
-        APrinterController(context.get(CONTROLLER)).respondPrint(command.sessionId, command.success);
+        (uint256 _sessionId,,,,function(uint256) external callback,) = APrinterStorage(context.get(STORAGE)).getReceiptPrint(_commandId);
+        APrinterController(context.get(CONTROLLER)).respondPrint(_sessionId, callback);
     }
 
     function confirmFailPrint(uint256 _commandId) public {
-        PrinterLib.ReceiptPrint memory command = database.retrieveReceiptPrint(_commandId);
-        APrinterController(context.get(CONTROLLER)).respondFailPrint(command.sessionId, command.fail);
+        (uint256 _sessionId,,,,,function(uint256) external callback) = APrinterStorage(context.get(STORAGE)).getReceiptPrint(_commandId);
+        APrinterController(context.get(CONTROLLER)).respondPrint(_sessionId, callback);
     }
 
 }
--- truffle/contracts/managers/api/APrinterManager.sol
+++ truffle/contracts/managers/api/APrinterManager.sol
@@ -2,38 +2,28 @@ pragma solidity 0.4.24;
 
 contract APrinterManager {
 
-    // @formatter:off
     function createReceipt(
         address _application,
         uint256 _sessionId,
         function(uint256, string memory, string memory) external _success,
         function(uint256) external _fail
     )
-        public
-        returns (
-            bool _accepted
-        );
-    // @formatter:on
+        public;
 
-    function confirmCreate(uint256 _commandId, string memory _receiptId, string memory _url) public;
+    function confirmCreate(uint256 _commandId, string _receiptId, string _url) public;
 
     function confirmFailCreate(uint256 _commandId) public;
 
-    // @formatter:off
     function printReceipt(
         address _application,
         uint256 _sessionId,
-        string memory _receiptId,
-        string memory _data,
-        string memory _params,
+        string _receiptId,
+        string _data,
+        string _params,
         function(uint256) external _success,
         function(uint256) external _fail
     )
-        public
-        returns (
-            bool _accepted
-        );
-    // @formatter:on
+        public;
 
     function confirmPrint(uint256 _commandId) public;
 
