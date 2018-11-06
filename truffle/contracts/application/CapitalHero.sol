62aec5fdae901f8bb99fec8c6ef1c13b1680231b
Andrew Ptasinskij <aptasinskij@gmail.com>
2018-11-06 10:31:00 +0200

    CapitalHero SC functions to call scan QR code

--- truffle/contracts/application/CapitalHero.sol
+++ truffle/contracts/application/CapitalHero.sol
@@ -2,6 +2,7 @@ pragma solidity 0.4.24;
 
 import "../platform/Context.sol";
 import "../controllers/api/public/CashInApi.sol";
+import "../controllers/api/public/CameraApi.sol";
 
 contract CapitalHero {
 
@@ -39,6 +40,33 @@ contract CapitalHero {
 
     function _balanceUpdate(uint256 _sessionId, uint256 _cashInId, uint256 _amount) public {
         emit CashInUpdate(_sessionId, _cashInId, _amount);
+    }
+
+    event ScanAccepted(uint256 _sessionId);
+    event SuccessQR(uint256 _sessionId, string _port, string _url, string _href);
+    event QRScanned(uint256 _sessionId, string _data);
+    event QRFail(uint256 _sessionId);
+
+    function scanQRWithLights(uint256 _sessionId) public {
+        CameraApi(context.get("camera-controller")).scanQRCodeWithLights(
+            _sessionId,
+            this.__successStartScan,
+            this.__QRScanned,
+            this.__QRFail
+        );
+        emit ScanAccepted(_sessionId);
+    }
+
+    function __successStartScan(uint256 _sessionId, string _port, string _url, string _href) public {
+        emit SuccessQR(_sessionId, _port, _url, _href);
+    }
+
+    function __QRScanned(uint256 _sessionId, string _data) public {
+        emit QRScanned(_sessionId, _data);
+    }
+
+    function __QRFail(uint256 _sessionId) public {
+        emit QRFail(_sessionId);
     }
 
 }
