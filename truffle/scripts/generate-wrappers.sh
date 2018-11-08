19c02e88cf9a25d6ab1cbaba7e0ed894300ae8a2
Andrew Ptasinskij <aptasinskij@gmail.com>
2018-11-08 10:21:55 +0200

    removed KioskStorage wrapper generation script

--- truffle/scripts/generate-wrappers.sh
+++ truffle/scripts/generate-wrappers.sh
@@ -3,7 +3,6 @@ web3j truffle generate build/contracts/ApplicationManager.json -p com.skysoft.va
 web3j truffle generate build/contracts/ApplicationStorage.json -p com.skysoft.vaultlogic.contracts -o ../contract-wrappers/src/main/java
 web3j truffle generate build/contracts/CapitalHero.json -p com.skysoft.vaultlogic.contracts -o ../contract-wrappers/src/main/java
 web3j truffle generate build/contracts/CashInOracle.json -p com.skysoft.vaultlogic.contracts -o ../contract-wrappers/src/main/java
-web3j truffle generate build/contracts/KioskStorage.json -p com.skysoft.vaultlogic.contracts -o ../contract-wrappers/src/main/java
 web3j truffle generate build/contracts/SessionOracle.json -p com.skysoft.vaultlogic.contracts -o ../contract-wrappers/src/main/java
 web3j truffle generate build/contracts/ParameterStorage.json -p com.skysoft.vaultlogic.contracts -o ../contract-wrappers/src/main/java
 web3j truffle generate build/contracts/CameraOracle.json -p com.skysoft.vaultlogic.contracts -o ../contract-wrappers/src/main/java
