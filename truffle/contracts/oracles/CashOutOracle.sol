18a62ebad7a31acb75e1907fdd0aec731d28d533
Imant Kukhar <imantkuhar93@gmail.com>
2018-11-15 17:24:41 +0200

    updated CashOutOracle api and it simple realization

--- truffle/contracts/oracles/CashOutOracle.sol
+++ truffle/contracts/oracles/CashOutOracle.sol
@@ -9,24 +9,43 @@ contract CashOutOracle is ACashOutOracle, Named("cash-out-oracle"), Mortal, Comp
 
     constructor(address _config) Component(_config) public {}
 
+    function onNextOpenCashOut(uint256 _commandId) public {
+        emit OpenCashOut(_commandId);
+    }
+
+    function successOpen(uint256 _commandId) public onlyOwner {
+    }
+    function failOpen(uint256 _commandId) public onlyOwner {
+    }
+
+
     function onNextValidateCashOut(uint256 _commandId, uint256 _sessionId, uint256 _toWithdraw, uint256[] _bills) public {
         emit ValidateCashOut(_commandId, _sessionId, _toWithdraw, _bills);
     }
 
-    /*function successValidate(uint256 _commandId) public onlyOwner {
+    function successValidate(uint256 _commandId) public onlyOwner {
+    }
+    function failValidate(uint256 _commandId) public onlyOwner {
     }
 
-    function failValidate(uint256 _commandId) public onlyOwner {
-    }*/
 
     function onNextCloseCashOut(uint256 _commandId, uint256 _sessionId, uint256 _toWithdraw, uint256[] _bills) public {
         emit CloseCashOut(_commandId, _sessionId, _toWithdraw, _bills);
     }
 
-    /*function successClose(uint256 _commandId) public {
+    function successClose(uint256 _commandId) public {
+    }
+    function failClose(uint256 _commandId) public {
     }
 
-    function failClose(uint256 _commandId) public {
-    }*/
+
+    function onNextRollbackCashOut(uint256 _commandId) public {
+        emit RollbackCashOut(_commandId);
+    }
+
+    function successRollback(uint256 _commandId) public onlyOwner {
+    }
+    function failRollback(uint256 _commandId) public onlyOwner {
+    }
 
 }
