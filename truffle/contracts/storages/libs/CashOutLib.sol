1f2778fce745701a8ebc117d71d1ea00b57723f7
Andrew Ptasinskij <aptasinskij@gmail.com>
2018-11-15 15:29:55 +0200

    defined CashOutLib structures

--- truffle/contracts/storages/libs/CashOutLib.sol
+++ truffle/contracts/storages/libs/CashOutLib.sol
@@ -3,6 +3,7 @@ pragma solidity 0.4.24;
 library CashOutLib {
 
     enum Status {
+        CREATING,
         ACTIVE,
         VALIDATING,
         ABLE_TO_CLOSE,
@@ -14,35 +15,34 @@ library CashOutLib {
     }
 
     struct Open {
-        uint256 sessionId;
-        uint256 amount;
-        function(uint256, uint256) external fail;
-        function(uint256, uint256) external success;
+        string kioskId;
+        function(string memory) external fail;
+        function(string memory, uint256) external success;
     }
 
     struct CashOut {
-        uint256 sessionId;
         address application;
         Status status;
     }
 
     struct Account {
+        uint256 toWithdraw;
+        uint256 VLFee;
+        uint256 reserve;
+        uint256[] fees;
+        address[] parties;
+    }
+
+    struct Validate {
+        uint256 sessionId;
+        function(uint256, uint256) external fail;
+        function(uint256, uint256) external success;
     }
 
     struct Close {
         uint256 sessionId;
         function(uint256, uint256) external fail;
         function(uint256, uint256) external success;
-    }
-
-    struct Split {
-        uint256[] fees;
-        address[] parties;
-    }
-
-    struct Cassette {
-        uint256[] bills;
-        uint256[] amounts;
     }
 
 }
