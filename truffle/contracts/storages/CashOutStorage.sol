6f88a6c17b475ebe93247c0962a10b0992257b5d
Imant Kukhar <imantkuhar93@gmail.com>
2018-11-19 16:11:58 +0200

    defined and implemented CashOutStorage missing retrieval and creating functions

--- truffle/contracts/storages/CashOutStorage.sol
+++ truffle/contracts/storages/CashOutStorage.sol
@@ -14,9 +14,11 @@ contract CashOutStorage is ACashOutStorage, Named("cash-out-storage"), Mortal, C
     mapping(uint256 => CashOutLib.Account) private channelAccount;
     mapping(uint256 => CashOutLib.Validate) private channelValidate;
     mapping(uint256 => CashOutLib.Close) private channelClose;
+    mapping(uint256 => CashOutLib.Rollback) private channelRollback;
 
     constructor(address _config) Component(_config) public {}
 
+    // @formatter:off
     function createCashOut(
         address _application
     )
@@ -27,7 +29,9 @@ contract CashOutStorage is ACashOutStorage, Named("cash-out-storage"), Mortal, C
     {
         _cashOutId = channels.push(CashOutLib.CashOut(_application, CashOutLib.Status.CREATING)) - 1;
     }
+    // @formatter:on
 
+    // @formatter:off
     function createOpen(
         uint256 _cashOutId,
         string _kioskId,
@@ -38,7 +42,9 @@ contract CashOutStorage is ACashOutStorage, Named("cash-out-storage"), Mortal, C
     {
         channelOpen[_cashOutId] = CashOutLib.Open(_kioskId, _fail, _success);
     }
+    // @formatter:on
 
+    // @formatter:off
     function createAccount(
         uint256 _cashOutId,
         uint256 _toWithdraw,
@@ -51,7 +57,9 @@ contract CashOutStorage is ACashOutStorage, Named("cash-out-storage"), Mortal, C
     {
         channelAccount[_cashOutId] = CashOutLib.Account(_toWithdraw, _VLFee, _reserve, _fees, _parties);
     }
+    // @formatter:on
 
+    // @formatter:off
     function createValidate(
         uint256 _cashOutId,
         uint256 _sessionId,
@@ -62,7 +70,9 @@ contract CashOutStorage is ACashOutStorage, Named("cash-out-storage"), Mortal, C
     {
         channelValidate[_cashOutId] = CashOutLib.Validate(_sessionId, _fail, _success);
     }
+    // @formatter:on
 
+    // @formatter:off
     function createClose(
         uint256 _cashOutId,
         uint256 _sessionId,
@@ -73,7 +83,21 @@ contract CashOutStorage is ACashOutStorage, Named("cash-out-storage"), Mortal, C
     {
         channelClose[_cashOutId] = CashOutLib.Close(_sessionId, _fail, _success);
     }
+    // @formatter:on
 
+    // @formatter:off
+    function createRollback(
+        uint256 _cashOutId,
+        function(uint256) external _fail,
+        function(uint256) external _success
+    )
+        public
+    {
+        channelRollback[_cashOutId] = CashOutLib.Rollback(_fail, _success);
+    }
+    // @formatter:on
+
+    // @formatter:off
     function retrieveCashOut(
         uint256 _cashOutId
     )
@@ -87,5 +111,98 @@ contract CashOutStorage is ACashOutStorage, Named("cash-out-storage"), Mortal, C
         _application = channels[_cashOutId].application;
         _status = channels[_cashOutId].status;
     }
+    // @formatter:on
+
+    // @formatter:off
+    function retrieveAccount(
+        uint256 _cashOutId
+    )
+        public
+        view
+        returns (
+            uint256 _toWithdraw,
+            uint256 _VLFee,
+            uint256 _reserve,
+            uint256[] _fees,
+            address[] _parties
+        )
+    {
+        _toWithdraw = channelAccount[_cashOutId].toWithdraw;
+        _VLFee = channelAccount[_cashOutId].VLFee;
+        _reserve = channelAccount[_cashOutId].reserve;
+        _fees = channelAccount[_cashOutId].fees;
+        _parties = channelAccount[_cashOutId].parties;
+    }
+    // @formatter:on
+
+    // @formatter:off
+    function retrieveOpen(
+        uint256 _cashOutId
+    )
+        public
+        view
+        returns (
+            string memory _kioskId,
+            function(string memory) external _fail,
+            function(string memory, uint256) external _success
+        )
+    {
+        _kioskId = channelOpen[_cashOutId].kioskId;
+        _fail = channelOpen[_cashOutId].fail;
+        _success = channelOpen[_cashOutId].success;
+    }
+    // @formatter:on
+
+    // @formatter:off
+    function retrieveValidate(
+        uint256 _cashOutId
+    )
+        public
+        view
+        returns (
+            uint256 _sessionId,
+            function(uint256, uint256) external _fail,
+            function(uint256, uint256) external _success
+        )
+    {
+        _sessionId = channelValidate[_cashOutId].sessionId;
+        _fail = channelValidate[_cashOutId].fail;
+        _success = channelValidate[_cashOutId].success;
+    }
+    // @formatter:on
+
+    // @formatter:off
+    function retrieveClose(
+        uint256 _cashOutId
+    )
+        public
+        view
+        returns (
+            uint256 _sessionId,
+            function(uint256, uint256) external _fail,
+            function(uint256, uint256) external _success
+        )
+    {
+        _sessionId = channelClose[_cashOutId].sessionId;
+        _fail = channelClose[_cashOutId].fail;
+        _success = channelClose[_cashOutId].success;
+    }
+    // @formatter:on
+
+    // @formatter:off
+    function retrieveRollback(
+        uint256 _cashOutId
+    )
+        public
+        view
+        returns (
+            function(uint256) external _fail,
+            function(uint256) external _success
+        )
+    {
+        _fail = channelRollback[_cashOutId].fail;
+        _success = channelRollback[_cashOutId].success;
+    }
+    // @formatter:on
 
 }
--- truffle/contracts/storages/api/ACashOutStorage.sol
+++ truffle/contracts/storages/api/ACashOutStorage.sol
@@ -4,22 +4,27 @@ import "../libs/CashOutLib.sol";
 
 contract ACashOutStorage {
 
+    // @formatter:off
     function createCashOut(
         address _application
     )
-    public
-    returns (
-        uint256 _cashOutId
-    );
+        public
+        returns (
+            uint256 _cashOutId
+        );
+    // @formatter:on
 
+    // @formatter:off
     function createOpen(
         uint256 _cashOutId,
         string _kioskId,
         function(string memory) external _fail,
         function(string memory, uint256) external _success
     )
-    public;
+        public;
+    // @formatter:on
 
+    // @formatter:off
     function createAccount(
         uint256 _cashOutId,
         uint256 _toWithdraw,
@@ -28,32 +33,113 @@ contract ACashOutStorage {
         uint256[] _fees,
         address[] _parties
     )
-    public;
+        public;
+    // @formatter:on
 
+    // @formatter:off
     function createValidate(
         uint256 _cashOutId,
         uint256 _sessionId,
         function(uint256, uint256) external _fail,
         function(uint256, uint256) external _success
     )
-    public;
+        public;
+    // @formatter:on
 
+    // @formatter:off
     function createClose(
         uint256 _cashOutId,
         uint256 _sessionId,
         function(uint256, uint256) external _fail,
         function(uint256, uint256) external _success
     )
-    public;
+        public;
+    // @formatter:on
 
+    // @formatter:off
+    function createRollback(
+        uint256 _cashOutId,
+        function(uint256) external _fail,
+        function(uint256) external _success
+    )
+        public;
+    // @formatter:on
+
+    // @formatter:off
     function retrieveCashOut(
         uint256 _cashOutId
     )
-    public
-    view
-    returns (
-        address _application,
-        CashOutLib.Status _status
-    );
+        public
+        view
+        returns (
+            address _application,
+            CashOutLib.Status _status
+        );
+    // @formatter:on
 
+    // @formatter:off
+    function retrieveAccount(
+        uint256 _cashOutId
+    )
+        public
+        view
+        returns (
+            uint256 _toWithdraw,
+            uint256 _VLFee,
+            uint256 _reserve,
+            uint256[] _fees,
+            address[] _parties
+        );
+    // @formatter:on
+
+    // @formatter:off
+    function retrieveOpen(
+        uint256 _cashOutId
+    )
+        public
+        view
+        returns (
+            string memory _kioskId,
+            function(string memory) external _fail,
+            function(string memory, uint256) external _success
+        );
+    // @formatter:on
+
+    // @formatter:off
+    function retrieveValidate(
+        uint256 _cashOutId
+    )
+        public
+        view
+        returns (
+            uint256 _sessionId,
+            function(uint256, uint256) external _fail,
+            function(uint256, uint256) external _success
+        );
+    // @formatter:on
+
+    // @formatter:off
+    function retrieveClose(
+        uint256 _cashOutId
+    )
+        public
+        view
+        returns (
+            uint256 _sessionId,
+            function(uint256, uint256) external _fail,
+            function(uint256, uint256) external _success
+        );
+    // @formatter:on
+
+    // @formatter:off
+    function retrieveRollback(
+        uint256 _cashOutId
+    )
+        public
+        view
+        returns (
+            function(uint256) external _fail,
+            function(uint256) external _success
+        );
+    // @formatter:on
 }
