fe3e54100bbac4d239edb8ed52017d7978317c17
Imant Kukhar <imantkuhar93@gmail.com>
2018-11-19 16:14:18 +0200

    implemented CashOutController::open, ::validate, ::close and ::rollback endpoints

--- truffle/contracts/controllers/CashOutController.sol
+++ truffle/contracts/controllers/CashOutController.sol
@@ -4,16 +4,15 @@ import "../platform/Named.sol";
 import "../platform/Mortal.sol";
 import "../platform/Component.sol";
 import "./api/ACashOutController.sol";
+import "../managers/api/ACashOutManager.sol";
 
 contract CashOutController is ACashOutController, Named("cash-out-controller"), Mortal, Component {
 
-    modifier onlyBy(string _name) {
-        require(msg.sender == context.get(_name), "illegal access");
-        _;
-    }
+    string constant MANAGER = "cash-out-manager";
 
     constructor(address _config) Component(_config) public {}
 
+    // @formatter:off
     function openCashOutChannel(
         string _kioskId,
         uint256 _toWithdraw,
@@ -23,29 +22,21 @@ contract CashOutController is ACashOutController, Named("cash-out-controller"), 
         function(string memory, uint256) external _success
     )
         external
-        //TODO:implementation: check if msg.sender is registered application
+    // @formatter:on
     {
+        //TODO:implementation: check that msg.sender is registered application, validate parameters
+        ACashOutManager(context.get(MANAGER)).openCashOutChannel(
+            msg.sender,
+            _kioskId,
+            _toWithdraw,
+            _fees,
+            _parties,
+            _fail,
+            _success
+        );
     }
 
-    function respondOpened(
-        string _kioskId,
-        uint256 _cashOutId,
-        function(string memory, uint256) external _callback
-    )
-        public
-    {
-        _callback(_kioskId, _cashOutId);
-    }
-
-    function respondFailOpen(
-        string _kioskId,
-        function(string memory) external _callback
-    )
-        public
-    {
-        _callback(_kioskId);
-    }
-
+    // @formatter:off
     function validateCashOutChannel(
         uint256 _sessionId,
         uint256 _cashOutId,
@@ -53,20 +44,19 @@ contract CashOutController is ACashOutController, Named("cash-out-controller"), 
         function(uint256, uint256) external _success
     )
         external
-        //TODO:implementation: check if msg.sender is registered application
+    // @formatter:on
     {
+        //TODO:implementation: check that msg.sender is registered application, validate parameters
+        ACashOutManager(context.get(MANAGER)).validateCashOutChannel(
+            msg.sender,
+            _sessionId,
+            _cashOutId,
+            _fail,
+            _success
+        );
     }
 
-    function respondValidated(
-        uint256 _sessionId,
-        uint256 _cashOutId,
-        function(uint256, uint256) external _callback
-    )
-        public
-    {
-        _callback(_sessionId, _cashOutId);
-    }
-
+    // @formatter:off
     function closeCashOutChannel(
         uint256 _sessionId,
         uint256 _cashOutId,
@@ -74,35 +64,90 @@ contract CashOutController is ACashOutController, Named("cash-out-controller"), 
         function(uint256, uint256) external _success
     )
         external
-        //TODO:implementation: check if msg.sender is registered application
+    // @formatter:on
     {
+        //TODO:implementation: check that msg.sender is registered application, validate parameters
+        ACashOutManager(context.get(MANAGER)).closeCashOutChannel(
+            msg.sender,
+            _sessionId,
+            _cashOutId,
+            _fail,
+            _success
+        );
     }
 
-    function respondClosed(
-        uint256 _sessionId,
-        uint256 _cashOutId,
-        function(uint256, uint256) external _callback
-    )
-        public
-    {
-        _callback(_sessionId, _cashOutId);
-    }
-
+    // @formatter:off
     function rollbackCashOutChannel(
         uint256 _cashOutId,
         function(uint256) external _fail,
         function(uint256) external _success
     )
         external
-        //TODO:implementation: check if msg.sender is registered application
+    // @formatter:on
     {
+        //TODO:implementation: check that msg.sender is registered application, validate parameters
+        ACashOutManager(context.get(MANAGER)).rollbackCashOutChannel(
+            msg.sender,
+            _cashOutId,
+            _fail,
+            _success
+        );
     }
 
+    // @formatter:off
+    function respondOpened(
+        string _kioskId,
+        uint256 _cashOutId,
+        function(string memory, uint256) external _callback
+    )
+        public
+    // @formatter:on
+    {
+        _callback(_kioskId, _cashOutId);
+    }
+
+    // @formatter:off
+    function respondFailOpen(
+        string _kioskId,
+        function(string memory) external _callback
+    )
+        public
+    // @formatter:on
+    {
+        _callback(_kioskId);
+    }
+
+    // @formatter:off
+    function respondValidated(
+        uint256 _sessionId,
+        uint256 _cashOutId,
+        function(uint256, uint256) external _callback
+    )
+        public
+    // @formatter:on
+    {
+        _callback(_sessionId, _cashOutId);
+    }
+
+    // @formatter:off
+    function respondClosed(
+        uint256 _sessionId,
+        uint256 _cashOutId,
+        function(uint256, uint256) external _callback
+    )
+        public
+    // @formatter:on
+    {
+        _callback(_sessionId, _cashOutId);
+    }
+
+    // @formatter:off
     function respondRolledBack(
         uint256 _cashOutId,
         function(uint256) external _callback
     )
         public
+    // @formatter:on
     {
         _callback(_cashOutId);
     }
