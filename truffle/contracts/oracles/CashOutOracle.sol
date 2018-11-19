45329b98e537a71641f9c99dae6a82025aa70d5e
Imant Kukhar <imantkuhar93@gmail.com>
2018-11-19 16:13:07 +0200

    implemented CashOutOracle contract

--- truffle/contracts/oracles/CashOutOracle.sol
+++ truffle/contracts/oracles/CashOutOracle.sol
@@ -4,8 +4,11 @@ import "../platform/Named.sol";
 import "../platform/Mortal.sol";
 import "../platform/Component.sol";
 import "./api/ACashOutOracle.sol";
+import "../managers/api/ACashOutManager.sol";
 
 contract CashOutOracle is ACashOutOracle, Named("cash-out-oracle"), Mortal, Component {
+
+    string constant MANAGER = "cash-out-manager";
 
     constructor(address _config) Component(_config) public {}
 
@@ -13,39 +16,48 @@ contract CashOutOracle is ACashOutOracle, Named("cash-out-oracle"), Mortal, Comp
         emit OpenCashOut(_commandId);
     }
 
-    function successOpen(uint256 _commandId) public onlyOwner {
-    }
-    function failOpen(uint256 _commandId) public onlyOwner {
+    function successOpen(uint256 _commandId) public {
+        ACashOutManager(context.get(MANAGER)).confirmOpen(_commandId);
     }
 
+    function failOpen(uint256 _commandId) public {
+        ACashOutManager(context.get(MANAGER)).confirmFailOpen(_commandId);
+    }
 
     function onNextValidateCashOut(uint256 _commandId, uint256 _sessionId, uint256 _toWithdraw, uint256[] _bills) public {
         emit ValidateCashOut(_commandId, _sessionId, _toWithdraw, _bills);
     }
 
-    function successValidate(uint256 _commandId) public onlyOwner {
-    }
-    function failValidate(uint256 _commandId) public onlyOwner {
+    function successValidate(uint256 _commandId) public {
+        ACashOutManager(context.get(MANAGER)).confirmValidate(_commandId);
     }
 
+    function failValidate(uint256 _commandId) public {
+        ACashOutManager(context.get(MANAGER)).confirmFailValidate(_commandId);
+    }
 
     function onNextCloseCashOut(uint256 _commandId, uint256 _sessionId, uint256 _toWithdraw, uint256[] _bills) public {
         emit CloseCashOut(_commandId, _sessionId, _toWithdraw, _bills);
     }
 
     function successClose(uint256 _commandId) public {
-    }
-    function failClose(uint256 _commandId) public {
+        ACashOutManager(context.get(MANAGER)).confirmClose(_commandId);
     }
 
+    function failClose(uint256 _commandId) public {
+        ACashOutManager(context.get(MANAGER)).confirmFailClose(_commandId);
+    }
 
     function onNextRollbackCashOut(uint256 _commandId) public {
         emit RollbackCashOut(_commandId);
     }
 
-    function successRollback(uint256 _commandId) public onlyOwner {
+    function successRollback(uint256 _commandId) public {
+        ACashOutManager(context.get(MANAGER)).confirmRollback(_commandId);
     }
-    function failRollback(uint256 _commandId) public onlyOwner {
+
+    function failRollback(uint256 _commandId) public {
+        ACashOutManager(context.get(MANAGER)).confirmFailRollback(_commandId);
     }
 
 }
--- truffle/contracts/oracles/api/ACashOutOracle.sol
+++ truffle/contracts/oracles/api/ACashOutOracle.sol
@@ -8,19 +8,27 @@ contract ACashOutOracle {
     event RollbackCashOut(uint256 _commandId);
 
     function onNextOpenCashOut(uint256 _commandId) public;
+
     function successOpen(uint256 _commandId) public;
+
     function failOpen(uint256 _commandId) public;
 
     function onNextValidateCashOut(uint256 _commandId, uint256 _sessionId, uint256 _toWithdraw, uint256[] _bills) public;
+
     function successValidate(uint256 _commandId) public;
+
     function failValidate(uint256 _commandId) public;
 
     function onNextCloseCashOut(uint256 _commandId, uint256 _sessionId, uint256 _toWithdraw, uint256[] _bills) public;
+
     function successClose(uint256 _commandId) public;
+
     function failClose(uint256 _commandId) public;
 
     function onNextRollbackCashOut(uint256 _commandId) public;
+
     function successRollback(uint256 _commandId) public;
+
     function failRollback(uint256 _commandId) public;
 
 }
