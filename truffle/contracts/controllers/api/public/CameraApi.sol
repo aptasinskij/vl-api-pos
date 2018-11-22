8ea72a52b4f4fda8a5d0fa9d1842f021fbb65772
Imant Kukhar <imantkuhar93@gmail.com>
2018-11-22 10:39:16 +0200

    improved CameraApi comments

--- truffle/contracts/controllers/api/public/CameraApi.sol
+++ truffle/contracts/controllers/api/public/CameraApi.sol
@@ -2,11 +2,16 @@ pragma solidity 0.4.24;
 
 interface CameraApi {
 
-    ///@dev function to trigger QR code scanning with lights
-    ///@param _sessionId - id of the session in which QR must occur
-    ///@param _success - callback function for informing about successful case of enabling camera
-    ///@param _scanned - callback function for informing about actual QR scanned data
-    ///@param _fail - callback function for informing about failed case of enabling camera
+    /**
+    @notice Scan QR code with lights
+    @dev function to trigger QR code scanning with lights
+    @param _sessionId - id of the session in which QR must occur
+    @param _success - callback function(_commandId, _port, _url, _href) for informing about successful case of enabling camera
+    @param _scanned - callback function(_sessionId, _qr) for informing about actual QR scanned data
+    @param _fail - callback function(_commandId) for informing about failed case of enabling camera
+    @return {
+    }
+    */
     // @formatter:off
     function scanQRCodeWithLights(
         uint256 _sessionId,
@@ -17,11 +22,16 @@ interface CameraApi {
         external;
     // @formatter:on
 
-    ///@dev function to trigger QR code scanning without lights
-    ///@param _sessionId - id of the session in which QR must occur
-    ///@param _success - callback function for informing about successful case of enabling camera
-    ///@param _scanned - callback function for informing about actual QR scanned data
-    ///@param _fail - callback function for informing about failed case of enabling camera
+    /**
+    @notice Scan QR code without lights
+    @dev function to trigger QR code scanning without lights
+    @param _sessionId - id of the session in which QR must occur
+    @param _success - callback function(_commandId, _port, _url, _href) for informing about successful case of enabling camera
+    @param _scanned - callback function(_sessionId, _qr) for informing about actual QR scanned data
+    @param _fail - callback function(_commandId) for informing about failed case of enabling camera
+    @return {
+    }
+    */
     // @formatter:off
     function scanQRCodeWithoutLights(
         uint256 _sessionId,
@@ -32,10 +42,15 @@ interface CameraApi {
         external;
     // @formatter:on
 
-    ///@dev function to disable QR scanning
-    ///@param _sessionId - id of the session in which camera must be disabled
-    ///@param _success - callback function to inform about camera successful disabling
-    ///@param _fail - callback function to inform about camera disabling fail
+    /**
+    @notice Stop QR code scanning
+    @dev function to disable QR scanning
+    @param _sessionId - id of the session in which camera must be disabled
+    @param _success - callback function(_sessionId) to inform about camera successful disabling
+    @param _fail - callback function(_sessionId) to inform about camera disabling fail
+    @return {
+    }
+    */
     // @formatter:off
     function stopQRScanning(
         uint256 _sessionId,
