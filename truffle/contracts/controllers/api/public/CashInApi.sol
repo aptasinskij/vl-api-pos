b8f263539a420304c2e5775897b816963744214c
Andrew Ptasinskij <aptasinskij@gmail.com>
2018-11-22 09:59:09 +0200

    improved CashInApi comments

--- truffle/contracts/controllers/api/public/CashInApi.sol
+++ truffle/contracts/controllers/api/public/CashInApi.sol
@@ -16,13 +16,14 @@ interface CashInApi {
 
 
     /**
-    @notice Create CashIn channel
-    @dev
-    @param _sessionId: id of the session in which CashIn channel creating
-    @param _maxBalance: the max amount of user's money which can be in CashIn channel
+    @notice Create CashIn channel. Require that the session does not has active CashIn channel.
+    @dev CashIn channel will be assigned status CREATING;
+        will proceed rather to ACTIVE or FAILED_TO_CREATE after successful or fail confirmation respectively
+    @param _sessionId: id of the session in which CashIn channel is creating
+    @param _maxBalance: max balance of the CashIn channel
     @param _fail: callback function(_sessionId) for informing about failed case of creating CashIn channel
-    @param _success: callback function(_sessionId, _maxBalance) for informing about successful case of creating CashIn channel
-    @param _update: callback function(_sessionId, _maxBalance, _amount) for informing about updating CashIn channel...
+    @param _success: callback function(_sessionId, _cashInId) for informing about successful case of creating CashIn channel
+    @param _update: callback function(_sessionId, _cashInId, _amount) for informing about updating CashIn channel balance
     @return {
     }
     */
@@ -38,12 +39,13 @@ interface CashInApi {
     // @formatter:on
 
     /**
-    @notice Close CashIn channel
-    @dev
-    @param _sessionId: id of the session in which CashIn channel closing
+    @notice Close CashIn channel. Require that the CashIn channel in ACTIVE state.
+    @dev CashIn channel will be assigned status CLOSE_REQUESTED;
+        will proceed rather to CLOSE or FAILED_TO_CLOSE after successful or fail confirmation respectively
+    @param _sessionId: id of the session in which CashIn channel is closing
     @param _channelId: id of the CashIn channel to close
-    @param _fees: list of fees for every application
-    @param _parties: list of addresses for every application
+    @param _fees: array of fee amount per split definition party
+    @param _parties: array of parties of split definition
     @param _success: callback function(_sessionId, _channelId) for informing about successful case of closing CashIn channel
     @param _fail: callback function(_sessionId, _channelId) for informing about failed case of closing CashIn channel
     @return {
@@ -64,7 +66,7 @@ interface CashInApi {
     /**
     @notice Get balance of the channel
     @dev
-    @param _channelId: id of the CashIn channel from which we want to get balance
+    @param _channelId: id of the CashIn channel
     @return {
         "_balance": "The balance of the channel"
     }
