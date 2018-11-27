0a17e90a71357ca87754b25133b05d6795f7b5a4
Andrew Ptasinskij <aptasinskij@gmail.com>
2018-11-27 10:44:40 +0200

    commented CashOut library definition in Libraries.sol

--- truffle/contracts/libs/Libraries.sol
+++ truffle/contracts/libs/Libraries.sol
@@ -229,10 +229,10 @@ library ApplicationLib {
 
 }*/
 
-library CashOutLib {
+/*library CashOutLib {
 
     using ApplicationLib for address;
-    /*using KioskLib for address;*/
+    *//*using KioskLib for address;*//*
 
     bytes32 constant CASH_OUT_INDEX = keccak256(abi.encode("CashOutIndex"));
 
@@ -366,10 +366,10 @@ library CashOutLib {
         splitSize = Database(self).getUintValue(string256(SPLIT_SIZE, index));
     }
 
-    /*function retrieveCashOutKiosk(address self, uint256 index) internal view returns (KioskLib.Kiosk memory) {
+    *//*function retrieveCashOutKiosk(address self, uint256 index) internal view returns (KioskLib.Kiosk memory) {
         require(cashOutExists(self, index), "Cash out channel not exists");
         return self.retrieveKiosk(getKioskId(self, index));
-    }*/
+    }*//*
 
     function setKioskId(address self, uint256 index, string kioskId) internal {
         Database(self).setStringValue(string256(KIOSK_ID, index), string(kioskId));
@@ -448,7 +448,7 @@ library CashOutLib {
         return keccak256(abi.encodePacked(field, index, counter));
     }
 
-}
+}*/
 
 library ParameterLib {
 
